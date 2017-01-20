7e919e2b074dc906444364effe7d82a1540d3119
Hana Shiro <shirohana0608@gmail.com>
2017-01-21 05:18:44 +0800

    Initial plugin

---
+++ src/main/java/me/shirohana/cardinal/Main.java
@@ -0,0 +1,13 @@
+package me.shirohana.cardinal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() { }
+
+    @Override
+    public void onDisable() { }
+
+}
