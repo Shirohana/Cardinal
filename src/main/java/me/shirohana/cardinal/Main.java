d7d2ca98bd92184aa935b97b326fb000db6a65ad
Hana Shiro <shirohana0608@gmail.com>
2017-01-21 09:17:05 +0800

    Plug BetterArrows module to main plugin

--- src/main/java/me/shirohana/cardinal/Main.java
+++ src/main/java/me/shirohana/cardinal/Main.java
@@ -2,10 +2,23 @@ package me.shirohana.cardinal;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.shirohana.cardinal.modules.betterarrows.BetterArrows;
+
 public final class Main extends JavaPlugin {
 
+    private final Cardinal cardinal;
+
+    public Main() {
+        cardinal = Cardinal.getInstance();
+    }
+
     @Override
-    public void onEnable() { }
+    public void onEnable() {
+        cardinal.setPlugin(this);
+        cardinal.plugModule(new BetterArrows(cardinal));
+
+        Cardinal.getRecipeManager().bake();
+    }
 
     @Override
     public void onDisable() { }
